7426213b902468a1d140d54ea7e121891facab25
chtmorris <agoosester@gmail.com>
2016-09-27 17:03:11 +0800

    Added two way data binding for titles

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+export class Dapp {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>Dapp Stack</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{dapp.name}} details</h2>
+    <div><label>id: </label>{{dapp.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="dapp.name" placeholder="name">
+    </div>
+  `
 })
 
-export class AppComponent { }
+export class AppComponent {
+  title = 'Dapps';
+  dapp: Dapp = {
+      id: 1,
+      name: 'Dapp 1'
+  }
+}
