55d07c3f7cdc1f23730a9a248836f8869d87c966
chtmorris <agoosester@gmail.com>
2016-09-27 22:04:54 +0800

    Added array of dapps

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,19 @@ export class Dapp {
   name: string;
 }
 
+const DAPPS: Dapp[] = [
+  { id: 11, name: 'Dapp Mr. Nice' },
+  { id: 12, name: 'Dapp Narco' },
+  { id: 13, name: 'Dapp Bombasto' },
+  { id: 14, name: 'Dapp Celeritas' },
+  { id: 15, name: 'Dapp Magneta' },
+  { id: 16, name: 'Dapp RubberMan' },
+  { id: 17, name: 'Dapp Dynama' },
+  { id: 18, name: 'Dapp Dr IQ' },
+  { id: 19, name: 'Dapp Magma' },
+  { id: 20, name: 'Dapp Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   template: `
