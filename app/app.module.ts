d5ec76cc359b38575163710572314642a612253c
chtmorris <agoosester@gmail.com>
2016-10-03 14:35:21 +0800

    Updated components to factor out dapp detail

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+
 import { AppComponent }  from './app.component';
+import { DappDetailComponent } from './dapp-detail.component';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    DappDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
