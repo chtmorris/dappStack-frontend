75c56e24a08b6c0e21f8481c9cc1063fd4fb9ff7
chtmorris <agoosester@gmail.com>
2016-10-03 15:19:30 +0800

    Moved the mock dapp data into separate file

---
+++ app/mock-dapps.ts
@@ -0,0 +1,14 @@
+import { Dapp } from './dapp';
+
+export const DAPPS: Dapp[] = [
+  { id: 11, name: 'Dapp Mr. Nice' },
+  { id: 12, name: 'Dapp Narco' },
+  { id: 13, name: 'Dapp Bombasto' },
+  { id: 14, name: 'Dapp Celeritas' },
+  { id: 15, name: 'Dapp Magneta' },
+  { id: 16, name: 'Dapp RubberMan' },
+  { id: 17, name: 'Dapp Dynama' },
+  { id: 18, name: 'Dapp Dr IQ' },
+  { id: 19, name: 'Dapp Magma' },
+  { id: 20, name: 'Dapp Tornado' }
+];
